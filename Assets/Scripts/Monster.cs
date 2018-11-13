b3860f546c32fd044f89198146d5fd6770da6337
ButtHead <vinicius.carlos@live.com>
2018-11-13 18:38:45 -0200

    pega posicao inicial quando inicia o jogo

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Monster
+{
+    /*private EnemyType type;
+
+    private Transform position;
+
+    public Monster(type)
+    {
+        this.GetType() = type;
+    }
+
+    public Enemy clone()
+    {
+        return new Enemy(type);
+    }*/
+}
+
+// Forma geral de uma classe singleton, que sempre ter apenas um, não pode criar mais objetos
+class Singleton
+{
+    /*private static Singleton instance;
+    
+    private Singleton() {}
+
+    public Singleton getInstance()
+    {
+        if (instance == null)
+        {
+            return new Singleton();
+        }
+
+        return instance;
+    }*/
+}
